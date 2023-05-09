1881680acab993bb6e49dc5dc0b8f8689a1744cf
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-05-09 01:17:03 +0000

    done

--- madlib_cli/a.py
+++
@@ -1,7 +0,0 @@
-def merge(stripped_txt,input_arr):
-
-    appnd = stripped_txt.format(*input_arr)
-    return appnd
-
-print(merge('It was a {} and {} {}.',['dark', 'stormy', 'night']))
-
--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -19,13 +19,11 @@ def welcome ():
     
 
 def read_template(dir):
-    
     with open (dir, "r") as temp_read:
-        text = str(temp_read.read())
-        return text
+        row_text = str(temp_read.read())
+        return row_text
     
 def parse_template(row_text):
-    print(type(row_text))
     stripped = re.sub("{\w*}", "{}" , row_text )
     parts = tuple(re.findall("\{(.*?)\}", row_text))
     return stripped , parts
@@ -34,5 +32,46 @@ def merge(stripped_txt,input_arr):
     appnd = stripped_txt.format(*input_arr)
     return appnd
 
+def write_txt_file(user_answer):
+    with open("result.txt" , "a") as result_file:
+        print(user_answer)
+        result_file.write(user_answer)
+
+def user_input (arr):
+    print (arr)
+    user_input=[]
+    for word in arr:
+        user_input.append(input(f"please inter {word} : ") )
+    return user_input
+
+
+
+
+
+
 if __name__ == '__main__':
+    dir ="assets/dark_and_stormy_night_template.txt"
+
+    row_text = read_template(dir)
+
+    print(row_text)
+
+    [stripped ,parts] = parse_template(row_text)
+
+    print(list(parts))
+
     welcome ()
+
+    user_input_words= user_input (list(parts))
+
+    print(user_input_words)
+
+    user_output = str(f"Your funny story --> {merge(stripped,user_input_words)}\n")
+
+    write_txt_file((user_output))
+
+
+
+
+
+
